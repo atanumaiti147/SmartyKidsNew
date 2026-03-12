5d42821e39a9cf54ffda6992c2744777f5e6a519
Atanu Maiti <vloggerraj840@gmail.com>
2026-03-12 20:03:16 +0530

    fix: updated nextjs to secure version

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-126246924-15f63",
-  "appId": "1:1088528812208:web:da415ccf5e8a5a1e219375",
-  "apiKey": "AIzaSyDb6wNNMJaVcOTf8G0yj4N9pURvIL4D248",
-  "authDomain": "studio-126246924-15f63.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1088528812208"
+  apiKey: "AIzaSyDb6wNNMJaVcOTf8G0yj4N9pURvIL4D248",
+  authDomain: "studio-126246924-15f63.firebaseapp.com",
+  databaseURL: "https://studio-126246924-15f63-default-rtdb.firebaseio.com",
+  projectId: "studio-126246924-15f63",
+  storageBucket: "studio-126246924-15f63.firebasestorage.app",
+  messagingSenderId: "1088528812208",
+  appId: "1:1088528812208:web:da415ccf5e8a5a1e219375"
 };
